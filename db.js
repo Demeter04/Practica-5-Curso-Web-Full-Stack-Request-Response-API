f6ef3560dc916244f33e53602a3cc07d5017d258
Oscar Urrego <osureg@gmail.com>
2024-06-04 11:56:15 -0500

    URI .ENV

--- db.js
+++ db.js
@@ -3,7 +3,7 @@
 
 // NUEVA FORMA ECMAScript 6
 import { MongoClient, ServerApiVersion } from 'mongodb';
-const uri = "mongodb+srv://Oscar_Urrego:oscar1234@curso-web-full-stack.roft1hd.mongodb.net/?retryWrites=true&w=majority&appName=Curso-Web-Full-Stack";
+const uri = process.env.MONGODB_URI
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
